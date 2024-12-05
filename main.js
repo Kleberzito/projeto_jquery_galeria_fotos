843b07b6b40a133db9620f24f006faaa4fbeade1
Kleber G. da Silva <kleber.gsilva@outlook.com>
2024-12-04 21:47:52 -0300

    feat: adicionar e remover imagens na galeria

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 $(document).ready(function () {
     $("#newImageButton").click(function () {
-        $("form").slideDown();
-    })
-
-    $("#cancelButton").click(function () {
-        $("form").slideUp();
-    })
+        if ($(this).hasClass("cancelButton")) {
+            $("form").slideUp();
+            $(this).removeClass("cancelButton").text("Nova imagem +");
+        } else {
+            $("form").slideDown();
+            $(this).addClass("cancelButton").text("Cancelar");
+        }
+    });
 
     $("form").on("submit", function (event) {
         event.preventDefault();
+
+        const urlImagem = $("#url-img").val();
+        const novoItem = $("<li style=\"display: none\"></li>");
+        $(`<img src="${urlImagem}" />`).appendTo(novoItem);
+        $(`<div class="overlay-imagem-link"><a href="${urlImagem}" target="_blank" title="ver imagem">ver imagem</a><button class="remove-button" type="button">X</button></div>`).appendTo(novoItem);
+        $(novoItem).appendTo("ul");
+        $(novoItem).fadeIn(1000);
+        $("#url-img").val("");
+    });
+
+    $(document).on("click", ".remove-button", function () {
+        $(this).closest("li").fadeOut(500, function () {
+            $(this).remove();
+        });
     });
 });
+
+
